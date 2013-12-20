32fefc8d1aebdcf92c820d1613196884f0ab296d
Greg Anderson <greg.1.anderson@greenknowe.org>
2013-12-20 13:12:33 -0800

    Style the event calendar in wk_bartik

---
+++ htdocs/sites/all/themes/wk_bartik/template.php
@@ -0,0 +1,80 @@
+<?php
+
+function wk_bartik_preprocess_html(&$variables) {
+  if (theme_get_setting('page_style', FALSE)) {
+    $variables['classes_array'][] = 'page-style';
+  }
+  else {
+    $variables['classes_array'][] = 'borderless-page';
+  }
+}
+
+function wk_bartik_process_page(&$variables) {
+  if (isset($variables['node'])) {
+    $fn = 'wk_bartik_process_page__' . $variables['node']->type;
+    if (function_exists($fn)) {
+      $fn($variables);
+    }
+  }
+}
+
+function wk_bartik_process_page__cal_event(&$variables) {
+    $variables['title_prefix']['#markup'] = '<b>YES:</b>' . var_export(array_keys($variables), TRUE);
+  $node = $variables['node'];
+  $output = '';
+  foreach ($node->taxonomy_vocabulary_2[LANGUAGE_NONE] as $index => $term) {
+    if (isset($term['taxonomy_term'])) {
+      $view = field_view_field('taxonomy_term', $term['taxonomy_term'], 'field_arms');
+      if (!empty($view)) {
+        $output .= render($view);
+      }
+    }
+  }
+  if (!empty($output)) {
+    $variables['title_prefix']['#markup'] = "
+<div class='calendar-event-arms'>
+  <div class='left-arms'>$output</div>
+  <div class='right-arms'>$output</div>
+</div>";
+  }
+}
+
+/**
+ * Override or insert variables into the node template.
+ */
+function wk_bartik_preprocess_node(&$variables) {
+  $fn = 'wk_bartik_preprocess_node__' . $variables['type'];
+  if (function_exists($fn)) {
+    $fn($variables);
+  }
+}
+
+function wk_bartik_preprocess_node__cal_event(&$variables) {
+  $node = $variables['node'];
+  //$variables['title_prefix']['#markup'] = '<b>WOW!</b>';
+  if (isset($node->field_date[LANGUAGE_NONE][0]['rrule'])) {
+    // Repeating events:  special checking for "weekly", then make our
+    // own formatted string.  Question: could we do this with a date format?
+    if (substr($node->field_date[LANGUAGE_NONE][0]['rrule'], 0, 28) == "RRULE:FREQ=WEEKLY;INTERVAL=1") {
+      // See http://drupal.org/node/1108164 for other options.
+      $date = new DateObject($node->field_date[LANGUAGE_NONE][0]['value'],'UTC',DATE_FORMAT_ISO);
+      $variables['cal_event_date'] = array('#markup' => '<div class="field-name-field-date">' . format_date($date->format('U'),'custom','\E\v\e\r\y l \a\t g:ia') . '</div>');
+    }
+  }
+  else {
+    // Non-repeating dates:  just use the default view mode, and print the
+    // date with the format defined for 'field_date'.
+    $variables['cal_event_date'] = field_view_field('node', $node, 'field_date', 'default');
+  }
+  $variables['cal_event_branch'] = field_view_field('node', $node, 'taxonomy_vocabulary_2', 'default'); // $node->taxonomy_vocabulary_2;
+  if (isset($node->field_event_site[LANGUAGE_NONE][0]['node']) && isset($node->field_event_site[LANGUAGE_NONE][0]['node']->field_location[LANGUAGE_NONE][0])) {
+    $city = $node->field_event_site[LANGUAGE_NONE][0]['node']->field_location[LANGUAGE_NONE][0]['city'];
+    $province = $node->field_event_site[LANGUAGE_NONE][0]['node']->field_location[LANGUAGE_NONE][0]['province'];
+    $province_name = $node->field_event_site[LANGUAGE_NONE][0]['node']->field_location[LANGUAGE_NONE][0]['province_name'];
+    if (!empty($city) && !empty($province)) {
+      $variables['cal_event_location_summary'] = '(' . $city . ', ' . $province . ')';
+      $variables['cal_event_location_long_summary'] =  $city . ', ' . $province_name;
+    }
+  }
+}
+
---
+++ htdocs/sites/all/themes/wk_bartik/templates/node--location.tpl.php
@@ -0,0 +1,163 @@
+<?php
+
+/**
+ * @file
+ * Bartik's theme implementation to display a node.
+ *
+ * Available variables:
+ * - $title: the (sanitized) title of the node.
+ * - $content: An array of node items. Use render($content) to print them all,
+ *   or print a subset such as render($content['field_example']). Use
+ *   hide($content['field_example']) to temporarily suppress the printing of a
+ *   given element.
+ * - $user_picture: The node author's picture from user-picture.tpl.php.
+ * - $date: Formatted creation date. Preprocess functions can reformat it by
+ *   calling format_date() with the desired parameters on the $created variable.
+ * - $name: Themed username of node author output from theme_username().
+ * - $node_url: Direct url of the current node.
+ * - $display_submitted: Whether submission information should be displayed.
+ * - $submitted: Submission information created from $name and $date during
+ *   template_preprocess_node().
+ * - $classes: String of classes that can be used to style contextually through
+ *   CSS. It can be manipulated through the variable $classes_array from
+ *   preprocess functions. The default values can be one or more of the
+ *   following:
+ *   - node: The current template type, i.e., "theming hook".
+ *   - node-[type]: The current node type. For example, if the node is a
+ *     "Blog entry" it would result in "node-blog". Note that the machine
+ *     name will often be in a short form of the human readable label.
+ *   - node-teaser: Nodes in teaser form.
+ *   - node-preview: Nodes in preview mode.
+ *   The following are controlled through the node publishing options.
+ *   - node-promoted: Nodes promoted to the front page.
+ *   - node-sticky: Nodes ordered above other non-sticky nodes in teaser
+ *     listings.
+ *   - node-unpublished: Unpublished nodes visible only to administrators.
+ * - $title_prefix (array): An array containing additional output populated by
+ *   modules, intended to be displayed in front of the main title tag that
+ *   appears in the template.
+ * - $title_suffix (array): An array containing additional output populated by
+ *   modules, intended to be displayed after the main title tag that appears in
+ *   the template.
+ *
+ * Other variables:
+ * - $node: Full node object. Contains data that may not be safe.
+ * - $type: Node type, i.e. story, page, blog, etc.
+ * - $comment_count: Number of comments attached to the node.
+ * - $uid: User ID of the node author.
+ * - $created: Time the node was published formatted in Unix timestamp.
+ * - $classes_array: Array of html class attribute values. It is flattened
+ *   into a string within the variable $classes.
+ * - $zebra: Outputs either "even" or "odd". Useful for zebra striping in
+ *   teaser listings.
+ * - $id: Position of the node. Increments each time it's output.
+ *
+ * Node status variables:
+ * - $view_mode: View mode, e.g. 'full', 'teaser'...
+ * - $teaser: Flag for the teaser state (shortcut for $view_mode == 'teaser').
+ * - $page: Flag for the full page state.
+ * - $promote: Flag for front page promotion state.
+ * - $sticky: Flags for sticky post setting.
+ * - $status: Flag for published status.
+ * - $comment: State of comment settings for the node.
+ * - $readmore: Flags true if the teaser content of the node cannot hold the
+ *   main body content.
+ * - $is_front: Flags true when presented in the front page.
+ * - $logged_in: Flags true when the current user is a logged-in member.
+ * - $is_admin: Flags true when the current user is an administrator.
+ *
+ * Field variables: for each field instance attached to the node a corresponding
+ * variable is defined, e.g. $node->body becomes $body. When needing to access
+ * a field's raw values, developers/themers are strongly encouraged to use these
+ * variables. Otherwise they will have to explicitly specify the desired field
+ * language, e.g. $node->body['en'], thus overriding any language negotiation
+ * rule that was previously applied.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_node()
+ * @see template_process()
+ */
+?>
+<article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?> role="article">
+
+  <?php print render($title_prefix); ?>
+  <?php if (!$page): ?>
+    <h2<?php print $title_attributes; ?>>
+      <a href="<?php print $node_url; ?>"><?php print $title; ?></a>
+    </h2>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+
+  <?php if ($display_submitted): ?>
+    <footer class="meta submitted">
+      <?php print $user_picture; ?>
+      <?php print $submitted; ?>
+    </footer>
+  <?php endif; ?>
+
+  <div class="content clearfix"<?php print $content_attributes; ?>>
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      hide($content["field_place_name"]);
+
+      // Check to see if there are any events in the past.  If so, hide them.
+
+      $past_events = array();
+      if (isset($content["field_site_events"]["#items"])) {
+        $has_upcoming_events = FALSE;
+        $now = time();
+        foreach($content["field_site_events"]["#items"] as $index => $item) {
+          if (isset($item["node"])) {
+            $start_date_str = $item["node"]->field_date[LANGUAGE_NONE][0]["value"];
+            $start_date_parsed = date_parse($start_date_str);
+            $start_date_mktime = mktime($start_date_parsed["hour"], $start_date_parsed["minute"], $start_date_parsed["second"], $start_date_parsed["month"], $start_date_parsed["day"], $start_date_parsed["year"]);
+            $start_date_description = date('F Y', $start_date_mktime);
+            if ($now > $start_date_mktime) {
+              $past_events[$start_date_mktime] = array(
+                "node" => $item["node"],
+                "when" => $start_date_description,
+              );
+              unset($content["field_site_events"]["#items"][$index]);
+            }
+            else {
+              $has_upcoming_events = TRUE;
+            }
+          }
+        }
+        if (!$has_upcoming_events) {
+          hide($content["field_site_events"]);
+        }
+      }
+
+      print render($content);
+    ?>
+  </div>
+
+  <?php
+    if (!empty($past_events)) {
+      print "<div class='field past-events'><div class='field-label'>Past events at this site:</div><ul>";
+      foreach ($past_events as $start_date_mktime => $item) {
+        print '<li>' . l($item["node"]->title, 'node/' . $item["node"]->nid) . ' ' . $start_date_description . '</li>';
+      }
+      print "</ul></div>";
+    }
+
+    // Remove the "Add new comment" link on the teaser page or if the comment
+    // form is being displayed on the same page.
+    if ($teaser || !empty($content['comments']['comment_form'])) {
+      unset($content['links']['comment']['#links']['comment-add']);
+    }
+    // Only display the wrapper div if there are links.
+    $links = render($content['links']);
+    if ($links):
+  ?>
+    <div class="link-wrapper">
+      <?php print $links; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php print render($content['comments']); ?>
+
+</article>
