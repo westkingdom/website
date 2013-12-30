58de3d335a7a6ddfbeaa16afbedf6eb8a8800cea
Greg Anderson <greg.1.anderson@greenknowe.org>
2013-12-29 19:25:59 -0800

    Make calendar view style meetings and demos differently than events

--- htdocs/sites/all/themes/wk_bartik/template.php
+++ htdocs/sites/all/themes/wk_bartik/template.php
@@ -9,3 +9,17 @@ function wk_bartik_preprocess_html(&$variables) {
   }
 }
 
+/**
+ * Implementation of hook_preprocess_calendar_item().
+ */
+function wk_bartik_preprocess_calendar_item(&$vars) {
+  // At the last possible minute we fix the values in rendered_fields so it
+  // contains the correct rendered content for the type of item and item display.
+  $item = $vars['item'];
+  $event_type = $item->entity->field_event_type[LANGUAGE_NONE][0]['value'];
+  $item->rendered = "<div class='calendar-event-type--$event_type'>" . $item->rendered . "</div>";
+
+  if (!empty($item->rendered) && empty($item->is_multi_day)) {
+    $vars['rendered_fields'] = array($item->rendered);
+  }
+}
