c559ecaf2574eb1e494f73ef0b217cb54d08da1b
Greg Anderson <greg.1.anderson@greenknowe.org>
2015-06-11 11:43:18 -0700

    Put misplaced 'default' directory back to htdocs/sites.

--- travis/install-on-travis.sh
+++ travis/install-on-travis.sh
@@ -13,6 +13,14 @@ mkdir -p $HOME/bin
 # Run composer install to install Drupal (pantheon-systems/drops-7),
 # all of our modules, and other components we need
 composer install
+if [ "$?" != "0" ]
+then
+  echo "Composer install failed." >&2
+  exit 1
+fi
+
+# Fix bug in our custom installer
+mv $TRAVIS_BUILD_DIR/htdocs/sites/sites/default $TRAVIS_BUILD_DIR/htdocs/sites/default
 
 # Do the settings.php shuffle for an empty settings.php
 # This prevents permissions issues with the sites/default directory
