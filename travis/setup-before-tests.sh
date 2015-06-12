8df0a23896c8d229fabb8546fe0014dc26d2c3da
Greg Anderson <greg.1.anderson@greenknowe.org>
2015-06-11 23:01:14 -0700

    Add some debug output so that we can see what is going on with the webserver.

--- travis/setup-before-tests.sh
+++ travis/setup-before-tests.sh
@@ -25,10 +25,12 @@ cp $PROJECT_BASE_DIR/htdocs/sites/default/default.settings.php $PROJECT_BASE_DIR
 # Use Drush to install Drupal and spin up PHP's built-in webserver
 cd $PROJECT_BASE_DIR/htdocs
 drush site-install -y standard --site-name="$SITE_NAME Travis Test Site" --db-url=mysql://root@localhost/drupal --account-name=admin --account-pass=admin
-drush runserver --server=builtin 8088 --strict=0 </dev/null &>/dev/null &
+drush runserver --server=builtin 8088 --strict=0 </dev/null &>$HOME/server.log &
 cd $PROJECT_BASE_DIR
 
 # Wait for a little while to let the webserver spin up
 echo "Waiting for the web server to finish spinning up."
-until netstat -an 2>/dev/null | grep '8088.*LISTEN'; do sleep 0.2; done
+sleep 1
+cat $HOME/server.log
+until netstat -an 2>/dev/null | grep '8088.*LISTEN'; do sleep 2; netstat -an ; tail -n 1 $HOME/server.log ; done
 echo "Got a response from our webserver; continuing."
