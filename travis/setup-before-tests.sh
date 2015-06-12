6571dcee642cecb760676dfe3fd214cbcae3d276
Greg Anderson <greg.1.anderson@greenknowe.org>
2015-06-11 22:49:40 -0700

    Don't run the built-in web server from a subshell, and detatch it from stdin, stdout and stderr when launching it.

--- travis/setup-before-tests.sh
+++ travis/setup-before-tests.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 SELF_DIRNAME="`dirname -- "$0"`"
-ROOT_PATH="`cd -P -- "$SELF_DIRNAME/.." && pwd -P`"
+PROJECT_BASE_DIR="`cd -P -- "$SELF_DIRNAME/.." && pwd -P`"
 
 # Set up our $PATH
 export PATH="$TRAVIS_BUILD_DIR/bin:$HOME/bin:$PATH"
 
 # Fix bug in our custom installer
-if [ -d $ROOT_PATH/htdocs/sites/sites/default ]
+if [ -d $PROJECT_BASE_DIR/htdocs/sites/sites/default ]
 then
-  mv $ROOT_PATH/htdocs/sites/sites/default $ROOT_PATH/htdocs/sites/default
+  mv $PROJECT_BASE_DIR/htdocs/sites/sites/default $PROJECT_BASE_DIR/htdocs/sites/default
 fi
 
-if [ ! -f $ROOT_PATH/htdocs/sites/default/default.settings.php ]
+if [ ! -f $PROJECT_BASE_DIR/htdocs/sites/default/default.settings.php ]
 then
   echo "No default.settings.php file; did you run composer install?"
   exit 1
@@ -20,14 +20,13 @@ fi
 
 # Do the settings.php shuffle for an empty settings.php
 # This prevents permissions issues with the sites/default directory
-cp $ROOT_PATH/htdocs/sites/default/default.settings.php $ROOT_PATH/htdocs/sites/default/settings.php
+cp $PROJECT_BASE_DIR/htdocs/sites/default/default.settings.php $PROJECT_BASE_DIR/htdocs/sites/default/settings.php
 
 # Use Drush to install Drupal and spin up PHP's built-in webserver
-(
-  cd $ROOT_PATH/htdocs
-  drush site-install -y standard --site-name="$SITE_NAME Travis Test Site" --db-url=mysql://root@localhost/drupal --account-name=admin --account-pass=admin
-  drush runserver --server=builtin 8088 --strict=0 &
-)
+cd $PROJECT_BASE_DIR/htdocs
+drush site-install -y standard --site-name="$SITE_NAME Travis Test Site" --db-url=mysql://root@localhost/drupal --account-name=admin --account-pass=admin
+drush runserver --server=builtin 8088 --strict=0 </dev/null &>/dev/null &
+cd $PROJECT_BASE_DIR
 
 # Wait for a little while to let the webserver spin up
 echo "Waiting for the web server to finish spinning up."
