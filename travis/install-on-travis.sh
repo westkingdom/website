7f847acde8e67a41303069c06203968553d1412d
Greg Anderson <greg.1.anderson@greenknowe.org>
2015-06-12 16:18:49 -0700

    Change order of policy file creation.

--- travis/install-on-travis.sh
+++ travis/install-on-travis.sh
@@ -3,18 +3,6 @@
 # Add a 'bin' directory
 mkdir -p $HOME/bin
 
-# Run composer install to install Drupal (pantheon-systems/drops-7),
-# all of our modules, and other components we need
-composer install
-if [ "$?" != "0" ]
-then
-  echo "Composer install failed." >&2
-  exit 1
-fi
-
-# Add vendor/bin and $HOME/bin to our $PATH
-export PATH="$TRAVIS_BUILD_DIR/bin:$HOME/bin:$PATH"
-
 # Create a local policy file that causes Travis to use 'drush 7' remotely.
 # n.b. we want to use drush8 for Drupal 8 sites
 mkdir -p "$HOME/.drush"
@@ -28,3 +16,15 @@ function policy_drush_sitealias_alter(&\$alias_record) {
   }
 }
 __EOF__
+
+# Run composer install to install Drupal (pantheon-systems/drops-7),
+# all of our modules, and other components we need
+composer install
+if [ "$?" != "0" ]
+then
+  echo "Composer install failed." >&2
+  exit 1
+fi
+
+# Add vendor/bin and $HOME/bin to our $PATH
+export PATH="$TRAVIS_BUILD_DIR/bin:$HOME/bin:$PATH"
