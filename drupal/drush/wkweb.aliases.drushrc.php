709b28019d2770ae4c00c3bf0ee0ebed42c2e914
Greg Anderson <greg.1.anderson@greenknowe.org>
2016-08-05 16:55:11 -0700

    Add aliases @wkweb.live and @wkweb.stage. Must cd to the Drupal root of the site to use.

---
+++ drupal/drush/wkweb.aliases.drushrc.php
@@ -0,0 +1,15 @@
+<?php
+
+$aliases['stage'] = array(
+  'remote-host' => 'server.westkingdom.org',
+  'remote-user' => 'www-dev',
+  'root' => '/srv/www/stage.westkingdom.org/drupal',
+  'uri' => 'http://stage.westkingdom.org',
+);
+
+$aliases['live'] = array(
+  'remote-host' => 'server.westkingdom.org',
+  'remote-user' => 'wkadmin',
+  'root' => '/srv/www/live.westkingdom.org/drupal',
+  'uri' => 'http://westkingdom.org',
+);
