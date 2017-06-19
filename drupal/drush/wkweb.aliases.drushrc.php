d85b4957b6740da5b61bb03b1a262746def0e0b1
Greg Anderson <greg.1.anderson@greenknowe.org>
2017-06-19 10:07:09 -0700

    Fix deploy to publish master to live on every merge.

--- drupal/drush/wkweb.aliases.drushrc.php
+++ drupal/drush/wkweb.aliases.drushrc.php
@@ -1,15 +1,15 @@
 <?php
 
 $aliases['stage'] = array(
-  'remote-host' => 'server.westkingdom.org',
-  'remote-user' => 'www-dev',
-  'root' => '/srv/www/stage.westkingdom.org/drupal',
-  'uri' => 'http://stage.westkingdom.org',
+  'remote-host' => 'virt.xalg.im',
+  'remote-user' => 'maint',
+  'root' => '/var/www/westkingdom/drupal',
+  'uri' => 'http://westkingdom.org',
 );
 
 $aliases['live'] = array(
-  'remote-host' => 'server.westkingdom.org',
-  'remote-user' => 'wkadmin',
-  'root' => '/srv/www/live.westkingdom.org/drupal',
+  'remote-host' => 'virt.xalg.im',
+  'remote-user' => 'maint',
+  'root' => '/var/www/westkingdom/drupal',
   'uri' => 'http://westkingdom.org',
 );
